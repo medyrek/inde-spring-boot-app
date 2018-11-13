a9503246d8f62aef9aae8b3c40605f95c8d11642
Mehdi Rekhroukh <mehdi.rekhroukh@bnc.ca>
2018-11-13 11:10:53 -0500

    init README

---
+++ src/main/java/com/edgenda/bnc/skillsmanager/model/Employee.java
@@ -0,0 +1,78 @@
+package com.edgenda.bnc.skillsmanager.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.data.annotation.PersistenceConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotEmpty
+    private String firstName;
+
+    @NotEmpty
+    private String lastName;
+
+    @Email
+    @NotEmpty
+    private String email;
+
+    @ManyToMany(mappedBy = "employees")
+    @JsonIgnoreProperties({"employees", "id", "description"})
+    private List<Skill> skills;
+
+    public Employee() {
+    }
+
+    public Employee(Long id, String firstName, String lastName, String email, List<Skill> skills) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.skills = skills;
+    }
+
+    @PersistenceConstructor
+    public Employee(String firstName, String lastName, String email, List<Skill> skills) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.skills = skills;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public List<Skill> getSkills() {
+        return skills;
+    }
+
+    @PreRemove
+    private void removeSkillsFromEmployee() {
+        for (Skill skill : skills) {
+            skill.getEmployees().remove(this);
+        }
+    }
+
+}
---
+++ src/main/java/com/edgenda/bnc/skillsmanager/model/Skill.java
@@ -0,0 +1,60 @@
+package com.edgenda.bnc.skillsmanager.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.data.annotation.PersistenceConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Skill {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotEmpty
+    private String name;
+
+    @NotEmpty
+    private String description;
+
+    @ManyToMany
+    @JoinTable(name = "EMPLOYEES_SKILLS")
+    @JsonIgnoreProperties("skills")
+    private List<Employee> employees;
+
+    public Skill() {
+    }
+
+    public Skill(Long id, String name, String description, List<Employee> employees) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.employees = employees;
+    }
+
+    @PersistenceConstructor
+    public Skill(String name, String description, List<Employee> employees) {
+        this.name = name;
+        this.description = description;
+        this.employees = employees;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+}
